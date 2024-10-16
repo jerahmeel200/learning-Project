a690cbcd1c34cd1ba251ea1d534ee052aedce497
jerahmeel princewill <jamicojerahmeel@gmail.com>
2024-10-16 15:53:17 +0100

    hashmap

--- hash-map/index.js
+++ hash-map/index.js
@@ -85,4 +85,112 @@ get(key){
 }
 
 // check if the map contains a specific key
+has(key){
+    return this.get(key) !== null
 }
+
+// remove a key-value pair
+remove(key){
+  const index = this.has(key)
+  this._checkIndex(index)
+  
+  const bucket = this.buckets[index]
+  if (!bucket) return false
+
+  const entryIndex = bucket.findIndex(([k])=> k === key)
+  if(entryIndex !== -1){
+    bucket.splice(entryIndex, 1)
+    this.size--
+    return true
+  }
+  return false
+}
+
+// Get the number of stored keys
+length(){
+    return this.size
+}
+// clear all  key-value pairs from the map
+clear(){
+    this.buckets = new Array( this.buckets.length)
+    this.size = 0
+}
+
+// Get all keys in the map
+keys(){
+    const keysArray = []
+    for (let bucket of this.buckets){
+        if(bucket){
+            for (let [key] of bucket){
+                keysArray.push(key)
+            }
+        }
+    }
+    return keysArray
+}
+
+// get all values in the map
+
+values(){
+    const valuesArray = []
+    for(let bucket of this.buckets){
+        if(bucket){
+            for(let [,value] of bucket){
+                valuesArray.push(value)
+            }
+        }
+    }
+    return valuesArray
+}
+
+// Get all key-value pairs as an array of [key, value] arrays
+
+entries(){
+    const entriesArray = []
+    for (let bucket of this.buckets){
+        if(bucket){
+            for (let [key, value] of bucket){
+                entriesArray.push([key, value])
+            }
+        }
+    }
+    return entriesArray
+}
+}
+
+
+
+// Test cases
+const test = new HashMap();
+
+// Adding initial values
+test.set('apple', 'red');
+test.set('banana', 'yellow');
+test.set('carrot', 'orange');
+test.set('dog', 'brown');
+test.set('elephant', 'gray');
+test.set('frog', 'green');
+test.set('grape', 'purple');
+test.set('hat', 'black');
+test.set('ice cream', 'white');
+test.set('jacket', 'blue');
+test.set('kite', 'pink');
+test.set('lion', 'golden');
+
+// Overwrite some values
+test.set('apple', 'green');
+test.set('banana', 'green');
+
+// Add another element to force rehashing
+test.set('moon', 'silver');
+
+// Test methods after resizing
+console.log(test.get('moon')); // 'silver'
+console.log(test.keys()); // All keys
+console.log(test.values()); // All values
+console.log(test.entries()); // All entries
+console.log(test.has('lion')); // true
+console.log(test.remove('kite')); // true
+console.log(test.length()); // 12 after removing one key
+test.clear();
+console.log(test.length()); // 0 after clearing all
