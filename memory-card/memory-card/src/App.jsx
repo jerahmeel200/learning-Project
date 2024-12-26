e30cf5477f4fa22571c39ee09b7c0eed0e8eb798
jerahmeel princewill <jamicojerahmeel@gmail.com>
2024-12-26 14:46:55 +0100

    feat: memory card

--- memory-card/memory-card/src/App.jsx
+++ memory-card/memory-card/src/App.jsx
@@ -1,54 +1,21 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
-import ScoreBoard from "./components/Scoreboard";
-import CardGrid from "./components/Cardgrid";
+// App.jsx
+
+import { useState } from "react";
 
 const App = () => {
-  const [currentScore, setCurrentScore] = useState(0);
-  const [bestScore, setBestScore] = useState(0);
-  const [cards, setCards] = useState([]);
-  const [ clickedCards, setClickedCards] = useState(new Set());
+  const [heading, setHeading] = useState("Magnificent Monkeys");
 
-  const apikey = "yDo8ddGBs5kk6PqEXvN5xRH5nVEBjsdN";
-
-  const shuffleArray = (array) => array.sort(() => Math.random() - 0.5);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch(
-          `https://api.giphy.com/v1/gifs/trending?api_key=${apikey}&limit=10`
-        );
-        const data = await res.json();
-        console.log("data", data);
-        setCards(shuffleArray(data.data)); // Update cards state
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  const handleCardClick = (cardId) => {
-    if(clickedCards.has(cardId)){
-      if(currentScore > bestScore){
-        setBestScore(currentScore)
-      }
-      setCurrentScore(0)
-      setClickedCards(new Set())
-    }else{
-      setClickedCards((prev)=> new Set(prev).add(cardId))
-      setCards(shuffleArray(cards))
-      setCurrentScore((prev)=> prev +1)
-    }
+  const clickHandler = () => {
+    setHeading("Radical Rhinos");
   };
 
   return (
-    <div>
-      <ScoreBoard currentScore={currentScore} bestScore={bestScore} />
-      <CardGrid cards={cards} onClickCard={(cardId) => handleCardClick(cardId)} />
-    </div>
+    <>
+      <button type="button" onClick={clickHandler}>
+        Click Me
+      </button>
+      <h1>{heading}</h1>
+    </>
   );
 };
 
