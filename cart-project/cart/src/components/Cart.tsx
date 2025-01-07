f88f7b3452aff287621ed2b76b4d03f2115f348c
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-01-07 13:56:15 +0100

    feat:fetched products from fake store api

--- cart-project/cart/src/components/Cart.tsx
+++ cart-project/cart/src/components/Cart.tsx
@@ -1,8 +1,10 @@
+import { TrashIcon } from "@heroicons/react/24/solid";
 import React from "react";
 
 type Product = {
   id: number;
-  name: string;
+  image: string;
+  title: string;
   price: number;
   quantity: number;
 };
@@ -19,46 +21,74 @@ const Cart: React.FC<CartProps> = ({
   updateQuantity,
 }) => {
   console.log("cartItems", cartItems);
+
   return (
-    <div>
-      <h2>Your Cart</h2>
-      {cartItems.length > 0 ? (
-        <ul>
-          {cartItems.map((item) => (
-            <>
-              <li key={item.id}>
-                {item.name} - ${item.price} x {item.quantity} = $
-                {item.price * item.quantity}
-              </li>
-              <p>Quantity: {item.quantity}</p>
+    <div className="px-4 sm:px-8 md:px-20 py-10">
+      <h2 className="text-gray-600 font-semibold text-lg sm:text-xl md:text-2xl mb-4">
+        Your Cart
+      </h2>
+      <div>
+        {cartItems.length > 0 ? (
+          <div className="space-y-5">
+            {cartItems.map((item) => (
+              <div
+                className="border border-gray-300 rounded-md shadow-lg flex flex-col sm:flex-row sm:justify-between items-center gap-4 p-6"
+                key={item.id}
+              >
+                <img
+                  className="w-20 sm:w-16 md:w-24 h-auto object-cover"
+                  src={item.image}
+                  alt={item.title}
+                />
 
-              <div style={{ marginRight: "10px" }}>
-                <button
-                  onClick={() => updateQuantity(item.id, false)}
-                  disabled={item.quantity <= 1}
+                <p className="text-sm sm:text-base font-medium text-gray-800 text-center">
+                  {item.title}
+                </p>
+
+                <p className="text-sm sm:text-base text-gray-600 text-center">
+                  ${item.price} x {item.quantity} = $
+                  {(item.price * item.quantity).toFixed(2)}
+                </p>
+
+                <div className="flex items-center gap-2">
+                  <button
+                    className="bg-blue-600 text-white font-bold px-2 py-1 rounded disabled:bg-blue-300"
+                    onClick={() => updateQuantity(item.id, false)}
+                    disabled={item.quantity <= 1}
+                  >
+                    -
+                  </button>
+                  <span className="text-sm sm:text-base">{item.quantity}</span>
+                  <button
+                    className="bg-blue-600 text-white font-bold px-2 py-1 rounded"
+                    onClick={() => updateQuantity(item.id, true)}
+                  >
+                    +
+                  </button>
+                </div>
+
+                <div
+                  className="cursor-pointer"
+                  onClick={() => removeFromCart(item.id)}
                 >
-                  -
-                </button>
-                <span style={{ margin: "0 10px" }}>{item.quantity}</span>
-                <button onClick={() => updateQuantity(item.id, true)}>+</button>
+                  <TrashIcon className="text-red-600 w-6 h-6 sm:w-8 sm:h-8" />
+                </div>
               </div>
-              <button onClick={() => removeFromCart(item.id)}>remove</button>
-            </>
-          ))}
-        </ul>
-      ) : (
-        <p>Your cart is empty.</p>
+            ))}
+          </div>
+        ) : (
+          <p className="text-gray-500 text-center">Your cart is empty.</p>
+        )}
+      </div>
+
+      {cartItems.length > 0 && (
+        <h2 className="text-gray-800 font-semibold text-lg sm:text-xl mt-6">
+          Total: $
+          {cartItems
+            .reduce((total, item) => total + item.price * item.quantity, 0)
+            .toFixed(2)}
+        </h2>
       )}
-
-      {
-        cartItems.length > 0 && (
-          <h2>
-            Total :$ 
-            {cartItems.reduce((total, item)=> total + item.price * item.quantity, 0).toFixed(2)}
-          </h2>
-
-        )
-      }
     </div>
   );
 };
--- cart-project/cart/src/components/Header.tsx
+++ cart-project/cart/src/components/Header.tsx
@@ -1,49 +1,51 @@
-import { BeakerIcon, ShoppingBagIcon } from "@heroicons/react/24/solid";
+import { ShoppingBagIcon } from "@heroicons/react/24/solid";
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-const Header = () => {
+
+interface HeaderProps {
+  totalQuantity: number;
+}
+
+const Header: React.FC<HeaderProps> = ({ totalQuantity }) => {
   const location = useLocation();
 
-  const isActive = (path: string) =>
+  console.log("quantities",totalQuantity)
+  const isActive = (path: string): string =>
     location.pathname === path ? "border-b-[3px] border-red-500" : "";
 
   const navLinks = [
-    {
-      path: "/home",
-      label: "Home",
-    },
-    {
-      path: "/shop",
-      label: "Shop",
-    },
-    {
-      path: "/cart",
-      label: "",
-    },
+    { path: "/", label: "Home" },
+    { path: "/shop", label: "Shop" },
+    // { path: "", label: "Cart" },
   ];
 
   return (
-    <div className="flex justify-between bg-slate-600 px-[40px] py-[10px]">
-      <div>Logo</div>
+    <div className="flex justify-between items-center bg-slate-600 px-10 py-3">
+      <div className="text-white font-bold text-lg">Logo</div>
 
-      <div className="flex gap-5">
-        <ul className="flex gap-5">
+      <div className="flex items-center gap-6">
+        <ul className="flex gap-6">
           {navLinks.map((link) => (
             <li key={link.path} className={`${isActive(link.path)}`}>
-              <Link className="font-medium" to={link.path}>
+              <Link
+                className="font-medium text-white hover:text-red-500"
+                to={link.path}
+              >
                 {link.label}
               </Link>
             </li>
           ))}
         </ul>
 
-        <div>
-          <div className="relative">
-            <ShoppingBagIcon className="size-6 text-black-500" />
-            <div className="absolute -top-2 bg-red-700 text-white rounded-full h-4 w-4  text-[10px] flex items-center justify-center">
-              10
-            </div>
-          </div>
+        <div className="relative">
+          <Link to="/cart">
+            <ShoppingBagIcon className="h-6 w-6 text-white" />
+            {totalQuantity > 0 && (
+              <div className="absolute -top-2 -right-2 bg-red-700 text-white rounded-full h-4 w-4 text-[10px] flex items-center justify-center">
+                {totalQuantity}
+              </div>
+            )}
+          </Link>
         </div>
       </div>
     </div>
@@ -51,16 +53,3 @@ const Header = () => {
 };
 
 export default Header;
-
-{
-  /* {
-        location.pathname === "/cart" ?
-        <Link to="/">
-        back
-        </Link>
-        :
-        <Link to="/cart">
-        Cart
-        </Link>
-       } */
-}
